e4a0e34151a4a0c994d5b9b3b9eb7c83adc43345
Ihor Borunov <borunov65@gmail.com>
2025-09-17 18:30:20 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
+import os
+import requests
+
+
+API_KEY = os.getenv("WEATHER_API_KEY")
+CITY = "Paris"
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+
+if not API_KEY:
+    raise ValueError(
+        "WEATHER_API_KEY not found in environment variables!"
+    )
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": CITY,
+        "lang": "en",
+        "aqi": "no"
+    }
+    response = requests.get(WEATHER_API_URL, params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    print(
+        f"Weather in {CITY}: {data['current']['temp_c']}°C, "
+        f"{data['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":
